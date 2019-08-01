141d631b14950e47800d06b989a5a0129c1baca0
buddi07 <42497975+Firewolf06@users.noreply.github.com>
2019-08-01 13:35:17 -0700

    Add files via upload

---
+++ Formatter.py
@@ -0,0 +1,19 @@
+file = open("input.txt", "r")
+loop = True
+out = ""
+while loop:
+    letter = file.read(1)
+    if letter == ":":
+        letter = ""
+        print("Removed ':'")
+    elif letter == "!":
+        loop = False
+        print("Found file end")
+    else:
+        print("Keeping letter '" + letter + "'")
+    out += letter
+
+outfile = open("output.txt", "w")
+outfile.write(out)
+outfile.close
+input("Press enter to exit")
